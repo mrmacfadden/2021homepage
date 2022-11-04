db5895ad6fc8cb249c48331ec6fcc021b59bf90b
Mike Macfadden <mikemacfadden@gmail.com>
2022-11-04 11:56:23 -0500

    Nov 4

--- myScripts.js
+++ myScripts.js
@@ -6,13 +6,25 @@ function startTime() {
   let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
 
 function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
   return i;
 }
 
-ß
+//POST-IT NOTE
+
+if (localStorage["note"]) {
+  var note = localStorage["note"];
+  document.getElementById("note").value = note;
+}
+
+function autoSave() {
+  var note = document.getElementById("note").value;
+  localStorage.setItem("note", note);
+}
